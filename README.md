0dcca3f85b430c4ce945dc92574ab4f4ad356655
Jon Love <124639155+jonnny013@users.noreply.github.com>
2024-02-08 13:41:19 +0800

    Add nodemailer

---
+++ README.md
@@ -0,0 +1,11 @@
+github:
+  backend: https://github.com/jonnny013/exercise2BE
+  frontend:
+
+website: 
+
+This is a simple system to send an email through the backend, the email is also saved to mongoDB.
+
+Node.js and express is used for the backend and react with MUI for the frontend.
+
+*** THIS PROJECT WILL NOTE CONNECT TO MONGODB FOR ANYONE ELSE WITHOUT MY PASSWORD, SORRY! ***
--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,14 +1,44 @@
-import Email from "../models/email.js"
+import Email from '../models/email.js'
+import nodemailer from 'nodemailer'
 
-const postEmail = async ({email}) => {
+const sendEmail = ({ email }) => {
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    },
+  })
+
+  let mailOptions = {
+    from: 'me',
+    to: email.email,
+    subject: email.subject,
+    text: email.body,
+  }
+
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log('Error ' + err)
+    } else {
+      console.log('Email sent successfully')
+    }
+  })
+}
+
+const postEmail = async ({ email }) => {
+  const something = await sendEmail({ email })
   const dateSent = new Date()
   const emailToSend = new Email({
     ...email,
     dateSent
   })
   const savedEmail = await emailToSend.save()
-  return savedEmail
-
+  return savedEmail 
 }
 
-export default {postEmail}
+export default { postEmail }
